932907d74f90efc73a46fc6c6954053793cf81e9
Feras <ferassaleh1980@gmail.com>
2022-11-14 23:09:12 +0100

    add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Portfolio from './components/portfolio/Portfolio';
 
 function App() {
     return (
@@ -13,7 +12,7 @@ function App() {
                 <Header />
                 <About />
                 <Experience />
-                <Portfolio />
+
                 <Footer />
             </>
         </div>
